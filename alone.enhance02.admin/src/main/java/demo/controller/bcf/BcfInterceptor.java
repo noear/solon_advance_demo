3cdc200edccead68502c4c29286bd3f4895e4154
noear <noear@live.cn>
2021-05-18 15:52:15 +0800

    1.4.0

--- alone.enhance02.admin/src/main/java/demo/controller/bcf/BcfInterceptor.java
+++ alone.enhance02.admin/src/main/java/demo/controller/bcf/BcfInterceptor.java
@@ -7,7 +7,6 @@ import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.core.handle.Context;
 import org.noear.water.WaterClient;
-import org.noear.water.utils.IPUtils;
 
 /**
  * @author noear 2021/2/16 created
@@ -32,16 +31,16 @@ public class BcfInterceptor extends BcfInterceptorBase {
             return;
         }
 
-//        if (Solon.cfg().isWhiteMode()) {
-//            if (ctx.uri().getHost().indexOf("localhost") < 0) {
-//                String ip = IPUtils.getIP(ctx);
-//                if (WaterClient.Whitelist.existsOfClientAndServerIp(ip) == false) {
-//                    ctx.output(ip + ",not is whitelist!");
-//                    ctx.setHandled(true);
-//                    return;
-//                }
-//            }
-//        }
+        if (Solon.cfg().isWhiteMode()) {
+            if (ctx.uri().getHost().indexOf("localhost") < 0) {
+                String ip = ctx.realIp();
+                if (WaterClient.Whitelist.existsOfClientAndServerIp(ip) == false) {
+                    ctx.output(ip + ",not is whitelist!");
+                    ctx.setHandled(true);
+                    return;
+                }
+            }
+        }
 
         super.verifyHandle(ctx);
     }
