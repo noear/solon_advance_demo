ff83a00787ee3f726e78a8c1b4921dd71d323cc8
noear <noear@live.cn>
2021-05-19 07:37:57 +0800

    1.4.1

--- alone.base03.rpc/alone.base03.provider/src/main/java/demo/controller/HelloServiceImp.java
+++ alone.base03.rpc/alone.base03.provider/src/main/java/demo/controller/HelloServiceImp.java
@@ -3,12 +3,13 @@ package demo.controller;
 import demo.HelloService;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.annotation.Remoting;
 
 /**
  * @author noear 2021/2/12 created
  */
 @Mapping("")
-@Component(remoting = true)
+@Remoting
 public class HelloServiceImp implements HelloService {
     @Override
     public String hello(String name) {
