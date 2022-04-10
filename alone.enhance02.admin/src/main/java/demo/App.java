322ce4b96891ba87413593f636fa759cf2efed74
noear <noear@live.cn>
2022-04-10 10:00:40 +0800

    1.7.1

--- alone.enhance02.admin/src/main/java/demo/App.java
+++ alone.enhance02.admin/src/main/java/demo/App.java
@@ -1,5 +1,6 @@
 package demo;
 
+import demo.controller.bcf.GritInterceptor;
 import org.noear.solon.Solon;
 
 /**
@@ -8,6 +9,7 @@ import org.noear.solon.Solon;
 public class App {
     public static void main(String[] args) {
         Solon.start(App.class, args ,app->{
+            app.before(new GritInterceptor());
             Config.tryInit();
         });
     }
---
+++ alone.enhance02.admin/src/main/java/demo/controller/bcf/GritInterceptor.java
@@ -0,0 +1,10 @@
+package demo.controller.bcf;
+
+import org.noear.grit.solon.GritPathInterceptorBase;
+
+/**
+ * @author noear 2021/2/16 created
+ */
+public class GritInterceptor extends GritPathInterceptorBase {
+
+}
