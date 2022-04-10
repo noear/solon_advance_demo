f36b215a7334e94f90dbbe4c3722bf0fa2067f5b
noear <noear@live.cn>
2022-04-10 09:58:02 +0800

    1.7.1

--- alone.enhance02.admin/src/main/java/demo/dso/Session.java
+++ alone.enhance02.admin/src/main/java/demo/dso/Session.java
@@ -1,45 +1,22 @@
 package demo.dso;
 
-import org.noear.bcf.BcfSessionBase;
-import org.noear.bcf.models.BcfUserModel;
-import org.noear.solon.Solon;
+import org.noear.grit.model.domain.Subject;
+import org.noear.grit.solon.SessionBase;
 
 /**
  * @author noear 2014-10-19
  * @since 1.0
  */
-public final class Session extends BcfSessionBase {
-    private static final Session _current = new Session();
-    public static Session current() {
-        return _current;
+public final class Session extends SessionBase {
+    private static Session current = new Session();
+    public static Session current(){
+        return current;
     }
-
 
     @Override
-    public String service() {
-        return Solon.cfg().appName();
-    }
-
-    //////////////////////////////////
-    //当前项目的扩展
-
-    @Override
-    public void loadModel(BcfUserModel user) throws Exception {
-        if (user == null) {
-            return;
-        }
-
-        setPUID(user.puid);
-        setUserId(user.user_id);
-        setUserName(user.cn_name);
-    }
-
-
-    public final String getValidation() {
-        return get("Validation_String", null);
-    }
-
-    public final void setValidation(String validation) {
-        set("Validation_String", validation.toLowerCase());
+    public void loadSubject(Subject subject) throws Exception {
+        setSubjectId(subject.subject_id);
+        setLoginName(subject.login_name);
+        setDisplayName(subject.display_name);
     }
 }
